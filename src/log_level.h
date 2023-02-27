7296616224e52afb6de47da2662bf153935983c6
shecannotsee <shecannotsee@163.com>
2023-02-27 16:13:57 +0800

    feat:添加日志等级

--- src/log_level.h
+++ src/log_level.h
@@ -15,14 +15,14 @@
 namespace sheLog {
 
 enum log_level : unsigned char {
-  ALL     = 0,
-  TRACE   = 1,
-  DEBUG   = 2,
-  INFO    = 3,
-  WARNING = 4,
-  ERROR   = 5,
-  FATAL   = 6,
-  OFF     = 7
+  ALL     = 0,  ///< Open all logs
+  TRACE   = 1,  ///<
+  DEBUG   = 2,  ///<
+  INFO    = 3,  ///< It can be used to output some important information of program operation in the production environment
+  WARNING = 4,  ///< Possible error
+  ERROR   = 5,  ///< Although an error event occurs, it still does not affect the continuous operation of the system
+  FATAL   = 6,  ///< Will cause the application to exit
+  OFF     = 7   ///< Close all logs
 };
 
 
