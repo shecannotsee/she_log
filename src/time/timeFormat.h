ad344c48ce54dd80f6666b12cc3048ee3c2bf6ce
shecannotsee <shecannotsee@163.com>
2023-02-28 14:45:43 +0800

    feat:日期格式化的修改

--- src/time/timeFormat.h
+++ src/time/timeFormat.h
@@ -15,30 +15,35 @@ namespace sheLog {
 namespace time {
 
 using ms_us = std::tuple<std::chrono::milliseconds,std::chrono::microseconds>;
-
-static std::map<std::string, std::string> month_map = {
-    {"Jan", "01"}, {"Feb", "02"}, {"Mar", "03"}, {"Apr", "04"},
-    {"May", "05"}, {"Jun", "06"}, {"Jul", "07"}, {"Aug", "08"},
-    {"Sep", "09"}, {"Oct", "10"}, {"Nov", "11"}, {"Dec", "12"}
-};
-
-
-ms_us get_ms() {
+ms_us get_ms_us() {
   auto now = std::chrono::system_clock::now();
   return std::make_pair(std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000,
                         std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()) % 1000);
 };
 
+std::string Supplement_0(int param) {
+  if (param>=0&&param<=9) {
+    return "0" + std::to_string(param);
+  } else {
+    return std::to_string(param);
+  }
+};
+
 std::string get_format_time() {
   auto now = std::chrono::system_clock::now();
-  std::time_t tm_1 = std::chrono::system_clock::to_time_t(now);
-  /*012(week) 456(month) 89(day) 11-18(time) 20-23(year)*/
-  std::string today = std::ctime(&tm_1);
+  // 将时间点转换为 time_t 类型
+  std::time_t t = std::chrono::system_clock::to_time_t(now);
+  // 将 time_t 类型转换为本地时间 struct tm
+  std::tm local_time = *std::localtime(&t);
   std::string ret;
-  ret += today.substr(20,4)+" ";
-  ret += month_map[today.substr(4,3)]+" ";
-  ret += today.substr(8,2)+" ";
-  ret += today.substr(11,8);
+  ret += Supplement_0(local_time.tm_year + 1900)+"-";
+  ret += Supplement_0(local_time.tm_mon + 1)+"-";
+  ret += Supplement_0(local_time.tm_mday)+" ";
+  ret += Supplement_0(local_time.tm_hour )+":";
+  ret += Supplement_0(local_time.tm_min )+":";
+  ret += Supplement_0(local_time.tm_sec );
+
+
   return ret;
 
 };
