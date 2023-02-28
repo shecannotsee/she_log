d97c3247026effb1262a65d4dfcdd59e04319852
shecannotsee <shecannotsee@163.com>
2023-02-28 15:05:55 +0800

    docs:添加时间函数的注释

--- src/time/timeFormat.h
+++ src/time/timeFormat.h
@@ -14,8 +14,21 @@ namespace sheLog {
 class time {
  public:
   using ms_us = std::tuple<std::chrono::milliseconds,std::chrono::microseconds>;
+  /**
+   * @brief Get the current number of milliseconds and microseconds
+   * 
+   */
   static ms_us get_ms_us();
+  /**
+   * @brief Fill 0 for single digit
+   * 
+   */
   static std::string Supplement_0(int param);
+  /**
+   * @brief Get the formatted date, for example:
+   * 1999-01-01 00:00:00
+   * 
+   */
   static std::string get_format_time();
 
 };// class time
