0c1c7b42a9b08488242d875a7990090da7c3fb21
shecannotsee <shecannotsee@163.com>
2023-03-01 15:37:56 +0800

    feat:添加使用用例

--- example/example.cpp
+++ example/example.cpp
@@ -1,4 +1,16 @@
 //
 // Created by shecannotsee on 2023/2/27.
 //
+#include <iostream>
 
+#include <interface.h>
+
+int main() {
+  std::cout<<"example test start.\n";
+
+  sheLog::interface p_log("log.txt");
+  p_log.DEBUG("hi");
+
+  std::cout<<"Done.\n";
+  return 0;
+};
