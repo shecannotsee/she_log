ee027b42b782e8a2f6d307581eba7921831f86c8
shecannotsee <shecannotsee@163.com>
2023-03-01 14:56:39 +0800

    test:简化测试代码

--- test/m5_interface_test.h
+++ test/m5_interface_test.h
@@ -6,15 +6,12 @@
 #define SHE_LOG_TEST_M5_INTERFACE_TEST_H_
 
 #include <interface.h>
-#include <chrono>
 
 namespace m5_interface_test {
 
 int main() {
   sheLog::interface p("./log.txt");
-
   p.DEBUG(sheLog::time::get_format_time());
-  p.shutdown();
   return 0;
 };
 
