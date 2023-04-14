21ee9bfef623ae1cd15a8c99329fe3591bcd7f59
shecannotsee <shecannotsee@163.com>
2023-04-14 11:07:54 +0800

    test:添加阻塞写入测试

---
+++ test/m7_async_write_real_time_test.h
@@ -0,0 +1,27 @@
+//
+// Created by shecannotsee on 23-4-13.
+//
+
+#ifndef SHE_LOG_TEST_M7_ASYNC_WRITE_REAL_TIME_TEST_H_
+#define SHE_LOG_TEST_M7_ASYNC_WRITE_REAL_TIME_TEST_H_
+
+#include <unistd.h>
+
+namespace m7_async_write_real_time_test {
+
+void main() {
+  try {
+    sheLog::interface log1("./a.log");
+    for (int i = 0; i < 10; ++i) {
+      log1.DEBUG("test");
+      sleep(1);
+    }
+
+  } catch (std::exception exc) {
+    std::cout << exc.what() <<std::endl;
+  }
+};
+
+};// namespace m7_async_write_real_time_test
+
+#endif //SHE_LOG_TEST_M7_ASYNC_WRITE_REAL_TIME_TEST_H_
--- test/main.cpp
+++ test/main.cpp
@@ -9,15 +9,17 @@
 #include "m4_queue_test.h"
 #include "m5_interface_test.h"
 #include "m6_write_speed_test.h"
+#include "m7_async_write_real_time_test.h"
 
 int main() {
   std::cout<< "Test start.\n";
 //  m1_she_log_exception_test::main();
 //  m2_Fopen_test::main();
-  m3_time_format_test::main();
+//  m3_time_format_test::main();
 //  m4_queue_test::main();
 //  m5_interface_test::main();
 //  m6_write_speed_test::main();
+  m7_async_write_real_time_test::main();
   std::cout<<"Test done.\n";
   return 0;
 };
