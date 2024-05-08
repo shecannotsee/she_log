d2bbab7f01393afa42193e0095e094111ad07233
shecannotsee <shecannotsee@163.com>
2024-05-08 11:32:51 +0800

    [chore] add cmake file

---
+++ cmake/she_log-config.cmake
@@ -0,0 +1,13 @@
+
+# ${she_log_FOUND}       : right get
+# ${she_log_INCLUDE_DIR} : include
+# ${she_log_LIBRARIES}   : lib
+# ${she_log_LINK_TARGET} : link target
+
+set(she_log "she_log")
+set(${she_log}_FOUND "1")
+set(${she_log}_INCLUDE_DIR   "${CMAKE_PREFIX_PATH}/include")
+set(${she_log}_LIBRARIES     "${CMAKE_PREFIX_PATH}/lib")
+set(${she_log}_LINK_TARGET   "she_log")
+
+
