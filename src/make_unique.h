e70711adeb4dd867d046a83eda4add451536ef08
shecannotsee <shecannotsee@163.com>
2024-05-08 12:01:34 +0800

    [refactor] make_unique support

--- src/make_unique.h
+++ src/make_unique.h
@@ -1,23 +1,17 @@
-//
-// Created by shecannotsee on 2023/2/27.
-//
-
 #ifndef SHE_LOG_SRC_MAKE_UNIQUE_H_
 #define SHE_LOG_SRC_MAKE_UNIQUE_H_
 
+#if __cplusplus >= 201103L && __cplusplus < 201402L
+
 #include <memory>
 
-namespace she_log {
-
-namespace CPP11 {
-
-template<typename T, typename... Ts>
-std::unique_ptr<T> make_unique(Ts &&... params) {
+namespace std {
+template <typename T, typename... Ts>
+std::unique_ptr<T> make_unique(Ts&&... params) {
   return std::unique_ptr<T>(new T(std::forward<Ts>(params)...));
-};
+}
+} // namespace std
 
-};// namespace CPP11 {
-
-};// namespace sheLog
+#endif
 
 #endif //SHE_LOG_SRC_MAKE_UNIQUE_H_
