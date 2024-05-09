dede5c59ee3fcfa198a32a801ccb57109316187b
shecannotsee <shecannotsee@163.com>
2024-05-09 17:16:25 +0800

    [feat] 1.Add header include

--- src/she_log.h
+++ src/she_log.h
@@ -1,4 +1,19 @@
 #ifndef SHE_LOG_H
 #define SHE_LOG_H
 
+#include "common/log_info.h"
+#include "common/make_unique.h"
+#include "common/time.h"
+#include "filter/log_level.h"
+#include "format/log_format.h"
+#include "logger/log_channel.h"
+#include "logger/logger.h"
+#include "output/console/console.h"
+#include "output/local_file/file_ops/file.h"
+#include "output/local_file/file_ops/seek.h"
+#include "output/local_file/file_ops/write.h"
+#include "output/local_file/local_file.h"
+#include "output/log_dest.h"
+#include "output/output.h"
+
 #endif  // SHE_LOG_H
