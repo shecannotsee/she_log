dbc3d255bd40fb2a5050e1221360ec5d9634bde5
shecannotsee <shecannotsee@163.com>
2024-05-09 17:16:11 +0800

    [chore] 1.Add the header file at the time of installation

--- cmake/z_install.cmake
+++ cmake/z_install.cmake
@@ -18,11 +18,57 @@ install(TARGETS
 
 if (generate_lib STREQUAL "ON")
     # include install
-    install(FILES
-        ${CMAKE_SOURCE_DIR}/src/base64.h # source head
+    install(FILES # /
+        ${CMAKE_SOURCE_DIR}/src/she_log.h # source head
         DESTINATION # to
         ${PROJECT_NAME}/include # target dir
         )
+    install(FILES # /common
+            ${CMAKE_SOURCE_DIR}/src/common/log_info.h
+            ${CMAKE_SOURCE_DIR}/src/common/make_unique.h
+            ${CMAKE_SOURCE_DIR}/src/common/time.h
+            DESTINATION # to
+            ${PROJECT_NAME}/include/common # target dir
+    )
+    install(FILES # /filter
+            ${CMAKE_SOURCE_DIR}/src/filter/log_level.h
+            DESTINATION # to
+            ${PROJECT_NAME}/include/filter # target dir
+    )
+    install(FILES # /format
+            ${CMAKE_SOURCE_DIR}/src/format/log_format.h
+            DESTINATION # to
+            ${PROJECT_NAME}/include/format # target dir
+    )
+    install(FILES # /logger
+            ${CMAKE_SOURCE_DIR}/src/logger/log_channel.h
+            ${CMAKE_SOURCE_DIR}/src/logger/logger.h
+            DESTINATION # to
+            ${PROJECT_NAME}/include/logger # target dir
+    )
+    install(FILES # /output
+            ${CMAKE_SOURCE_DIR}/src/output/output.h
+            ${CMAKE_SOURCE_DIR}/src/output/log_dest.h
+            DESTINATION # to
+            ${PROJECT_NAME}/include/output # target dir
+    )
+    install(FILES # /output/console
+            ${CMAKE_SOURCE_DIR}/src/output/console/console.h
+            DESTINATION # to
+            ${PROJECT_NAME}/include/output/console # target dir
+    )
+    install(FILES # /output/local_file
+            ${CMAKE_SOURCE_DIR}/src/output/local_file/local_file.h
+            DESTINATION # to
+            ${PROJECT_NAME}/include/output/local_file # target dir
+    )
+    install(FILES # /output/local_file/file_ops
+            ${CMAKE_SOURCE_DIR}/src/output/local_file/file_ops/file.h
+            ${CMAKE_SOURCE_DIR}/src/output/local_file/file_ops/seek.h
+            ${CMAKE_SOURCE_DIR}/src/output/local_file/file_ops/write.h
+            DESTINATION # to
+            ${PROJECT_NAME}/include/output/local_file/file_ops # target dir
+    )
     # lib install
     install(TARGETS
         ${library_static_name} ${library_dynamic_name}
