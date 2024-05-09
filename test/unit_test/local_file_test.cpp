6746c41108b3e021892efe8536545394157ccc8c
shecannotsee <shecannotsee@163.com>
2024-05-09 16:43:23 +0800

    [test] 1.adjust output dir

--- test/unit_test/local_file_test.cpp
+++ test/unit_test/local_file_test.cpp
@@ -1,33 +1,32 @@
-//
-// Created by shecannotsee on 24-5-8.
-//
-
-#include <output/local_file/file_ops//file.h>
-#include <output/local_file/file_ops//seek.h>
-#include <output/local_file/file_ops//write.h>
+#include <output/local_file/file_ops/file.h>
+#include <output/local_file/file_ops/seek.h>
+#include <output/local_file/file_ops/write.h>
 #include <she_test.h>
+#include <sys/stat.h>
 
 namespace {
-const std::string dir_path  = "./test";
-const std::string file_path = dir_path + "/file_test.txt";
+const std::string dir_path  = "./output";
+const std::string file_path = "./output/file_ops_file.txt";
+
+void create_directory(const std::string& dir_path) {
+  struct stat st = {0};
+
+  if (stat(dir_path.c_str(), &st) == -1) {
+    // 如果目录不存在，创建它
+    mkdir(dir_path.c_str(), 0700);
+  }
+}
 }  // namespace
 
 SHE_TEST(unit_test, file_ops_file) {
   using namespace she_log::detail;
-  auto ret = dir::exist(dir_path);
-  if (ret == false) {
-    printf("dir:%s not exist...\n", dir_path.c_str());
-    dir::create(dir_path);
-  }
-  ret = file::exist(file_path);
-  if (ret == false) {
-    printf("file:%s not exist...\n", file_path.c_str());
-  }
+  create_directory(dir_path);
 
   const write handle(file_path);
   handle.content("hello\n");
   handle.content("hello\n");
   handle.content("123421321321\n");
+  handle.content("done\n");
 
   return true;
 }
