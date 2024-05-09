47eea7c6e52164843e8d86fb841f5f4f17ca5a39
shecannotsee <shecannotsee@163.com>
2024-05-09 21:17:26 +0800

    [style] 1.version add \n

--- exec/application.cpp
+++ exec/application.cpp
@@ -3,6 +3,6 @@
 #include <iostream>
 
 int main() {
-  std::cout << she_log::fmt("she_log version is:{}", she_log::detail::version);
+  std::cout << she_log::fmt("she_log version is:{}\n", she_log::detail::version);
   return 0;
 }
